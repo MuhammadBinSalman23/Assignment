16323de4c0033470a1124588ed7153e938bc5e39
Muhammad Bin Salman <muhemmadbinsalman@gmail.com>
2026-03-31 16:50:29 +0500

    assigmentConversions

---
+++ Asgn2Mar.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+class Asgn2Mar {
+    
+public static void main(String[] args) {
+    
+    int dec, originalDec;
+    String binary="";
+    
+     Scanner sc = new Scanner(System.in);
+   dec = Integer.parseInt(sc.nextLine());
+   originalDec = dec;
+    /* while (dec != 0) {
+             
+           binary  =   (dec % 2) + binary;
+           dec  = dec / 2;
+        
+    }
+    // part(a)
+    System.out.println("Part(a) Decimal number "+originalDec+" its Binary is " +binary);
+    */
+     
+
+    /*
+     // part(b)
+    
+    int[] binaryArray =  convertDecToBinaryUsingArray(dec);
+    System.out.print("Part(b) Decimal number "+originalDec+" its Binary is " );
+    for(int i = 0; i < binaryArray.length ; i++ ){
+      System.out.print(binaryArray[i]);
+    }
+    */
+
+
+    
+    // part(c)
+    System.out.println("Part(c) Decimal number "+originalDec+" its Binary is "+convertDecToBinary(dec) ); 
+    
+
+    //convertDecToHexa();
+  
+
+} // main method ends
+
+
+public static int[] convertDecToBinaryUsingArray(int decpara ) {
+    int[] binary= new int[20]; 
+    int[] returnBinary;
+    int index = 0;
+    while (decpara != 0) {
+             
+           binary[index]  =   (decpara % 2);
+           decpara  = decpara / 2;
+           index++;
+        
+    }
+
+    returnBinary = new int[index]; 
+    for(int y=0, z=index-1; y < returnBinary.length; y++, z--) {
+      returnBinary[y] = binary[z]; 
+    }
+    return returnBinary;    
+ } // convertDecToBinaryUsingArray ends
+
+public static String convertDecToBinary(int decpara ) {
+    String binary =""; 
+    while (decpara != 0) {
+             
+           binary  =   (decpara % 2) + binary;
+           decpara  = decpara / 2;
+        
+    }
+ return binary;    
+ } // convertDecToBinary ends
+
+
+
+
+
+public static void convertDecToHexa() {
+
+int dec, originalDec;
+String hexaDecimal="";
+String remainder="";
+
+  Scanner sc = new Scanner(System.in);
+   dec = Integer.parseInt(sc.nextLine());
+   originalDec = dec;
+   while (dec !=0 ) {
+    remainder = String.valueOf( (dec % 16 ));
+
+   
+    switch (remainder) {
+    case "10":
+        remainder = "A" ;
+        break;
+    case "11":
+        System.out.println("Tuesday");
+        break;
+    case "15":
+        remainder = "F" ;
+        break;
+    default:
+                
+        break;
+}
+
+
+     /*if(remainder.equals("10")) {
+      remainder   = "A";
+     }
+     else if (remainder.equals("15")) {
+      remainder   = "F";
+     }*/
+
+
+     hexaDecimal  = remainder + hexaDecimal;
+    dec =  dec / 16;
+    
+   }
+   
+
+    
+    System.out.println(" Decimal number "+originalDec+" its Hexadecimal is " +hexaDecimal);
+} // convertDecToHexa method ends 
+
+
+
+} // class ends
